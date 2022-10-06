b81f98185753e733ac9d1e4350a344ba7a0c33af
Wesley Guerra <wesley.moreira@vetta.digital>
2022-10-05 23:42:02 -0300

    Ajuste post

---
+++ src/Components/Comment.jsx
@@ -0,0 +1,4 @@
+import styles from "./Comment.module.css";
+export function Comment() {
+  return <div></div>;
+}
--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,15 +1,17 @@
 import styles from "./Post.module.css";
 export function Post() {
   return (
-    <article className={StyleSheet.autho}>
+    <article className={styles.post}>
       <header>
-        <img
-          className={styles.avatar}
-          src="https://github.com/wesleygmssa.png"
-        />
-        <div className={styles.authorInfo}>
-          <strong>Wesley Guerra</strong>
-          <span>Web Developer</span>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/wesleygmssa.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Wesley Guerra</strong>
+            <span>Web Developer</span>
+          </div>
         </div>
 
         <time title="11 de Maio ás 08:13h" dateTime="2022-05-22 08:12:30">
@@ -17,19 +19,25 @@ export function Post() {
         </time>
       </header>
       <div className={styles.content}>
+        <p>Fala Galera 👋</p>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ipsam
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ipsam
+          👉 <a href="">Jane.design/doctorcare</a>
         </p>
         <p>
-          <a href="">Jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="">#novoprojeto #nlw #rockeatseat</a>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rockeatseat</a>
         </p>
       </div>
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Enviar</button>
+        </footer>
+      </form>
     </article>
   );
 }
