5e215310bc822e25ba2943081188ba31fbc3ffc7
Wesley Guerra <wesley.moreira@vetta.digital>
2022-10-08 00:38:17 -0300

    update

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -1,4 +1,32 @@
+import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 export function Comment() {
-  return <div></div>;
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/wesleygmssa.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Wesley Guerra</strong>
+              <time title="11 de Maio às 00:13" dataTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
