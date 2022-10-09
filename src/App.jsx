7d3fad5b074b44fb388ffceec1ce19385b01261e
Wesley Guerra <wesley.moreira@vetta.digital>
2022-10-09 14:34:15 -0300

    Working with data

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,59 @@ import { Post } from "./Components/Post";
 
 import "./global.css";
 
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala Galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ipsam",
+      },
+      {
+        type: "link",
+        content: "Jane.design/doctorcare",
+      },
+      {
+        type: "hashtag",
+        content: "#novoprojeto",
+      },
+    ],
+    publishedAt: new Date("2022-05-22 08:12:30"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk brito",
+      role: "Educator Rockeatseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala Galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ipsam",
+      },
+      {
+        type: "link",
+        content: "Jane.design/doctorcare",
+      },
+      {
+        type: "hashtag",
+        content: "#novoprojeto",
+      },
+    ],
+    publishedAt: new Date("2022-05-22 08:12:30"),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -12,7 +65,14 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {post.map((post) => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,34 +1,61 @@
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
-export function Post() {
+import { format, formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+// author: {avatar_url, name, role} },
+
+export function Post(props) {
+  const publishedDateFormatted = format(
+    props.publishedAt,
+    "d 'de' MMMM 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(props.publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+  // const publicationDate = new Intl.DateTimeFormat("pt-BR", {
+  //   day: "2-digit",
+  //   month: "long",
+  //   hour: "2-digit",
+  //   minute: "2-digit",
+  // }).format(props.publishedAt);
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/diego3g.png" />
+          <Avatar src={props.author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Wesley Guerra</strong>
-            <span>Web Developer</span>
+            <strong>{props.author.name}</strong>
+            <span>{props.author.role}</span>
           </div>
         </div>
 
-        <time title="11 de Maio ás 08:13h" dateTime="2022-05-22 08:12:30">
-          Publicado há 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={props.publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
       <div className={styles.content}>
-        <p>Fala Galera 👋</p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ipsam
-        </p>
-        <p>
-          👉 <a href="">Jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
-          <a href="">#rockeatseat</a>
-        </p>
+        {props.content.map((content, index) => {
+          if (content.type === "paragraph") {
+            return <p key={index}>{content.content}</p>;
+          } else if (content.type === "link") {
+            return (
+              <a key={index} href={content.content}>
+                {content.content}
+              </a>
+            );
+          }
+        })}
       </div>
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
